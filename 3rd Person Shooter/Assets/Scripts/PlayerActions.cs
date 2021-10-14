eb8a07f601ab47e299acc799447e641db4f93bcc
TelluriumTussi <carter.tussi2022@gmail.com>
2021-10-13 21:36:33 -0400

    3PS

---
+++ 3rd Person Shooter/Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
+public class PlayerActions : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform barrelTransform;
+    [SerializeField] private Transform bulletParent;
+
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private float bulletRange = 25;
+
+    private CharacterController controller;
+    private PlayerInput playerInput;
+    private Transform cameraTransform;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+
+    private InputAction moveAction, jumpAction;
+    private InputAction shootAction;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+        cameraTransform = Camera.main.transform;
+        moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
+        shootAction = playerInput.actions["Shoot"];
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnEnable()
+    {
+        shootAction.performed += _ => ShootGun();
+    }
+
+    private void OnDisable()
+    {
+        shootAction.performed -= _ => ShootGun();
+    }
+
+    void ShootGun()
+    {
+        RaycastHit hit;
+        GameObject bullet = GameObject.Instantiate(bulletPrefab, barrelTransform.position, Quaternion.identity, bulletParent);
+        BulletAction bulletAct = bullet.GetComponent<BulletAction>();
+        if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hit, Mathf.Infinity))
+        {
+            bulletAct.target = hit.point;
+            bulletAct.hit = true;
+        }
+        else
+        {
+            bulletAct.target = cameraTransform.position + cameraTransform.forward * bulletRange;
+            bulletAct.hit = false;
+        }
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        move = move.x * cameraTransform.right.normalized + move.z * cameraTransform.forward.normalized;
+        move.y = 0f;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // Changes the height position of the player..
+        if (jumpAction.triggered && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        // Rotate towards camera direction
+        Quaternion targetRotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ 3rd Person Shooter/Assets/Scripts/SwitchVCam.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Cinemachine;
+using System;
+
+public class SwitchVCam : MonoBehaviour
+{
+
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private int priorityBoost = 10;
+    [SerializeField] private Canvas thirdPersonCanvas, aimCanvas;
+
+    private CinemachineVirtualCamera vcam;
+    private InputAction aimAction;
+
+    private void Awake()
+    {
+        vcam = GetComponent<CinemachineVirtualCamera>();
+        aimAction = playerInput.actions["Aim"];
+        Cursor.visible = false;
+        aimCanvas.enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        aimAction.performed += _ => StartAim();
+        aimAction.canceled += _ => CancelAim();
+    }
+
+    private void StartAim()
+    {
+        vcam.Priority += priorityBoost;
+        aimCanvas.enabled = true;
+        thirdPersonCanvas.enabled = false;
+    }
+
+    private void CancelAim()
+    {
+        vcam.Priority -= priorityBoost;
+        aimCanvas.enabled = false;
+        thirdPersonCanvas.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        aimAction.performed -= _ => StartAim();
+        aimAction.canceled -= _ => CancelAim();
+    }
+}
